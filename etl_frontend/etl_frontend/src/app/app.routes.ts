9185733b721d63c9abd82c6ee84246a4ed347057
Mohammad Amin Dadkhah <mohammadamin.dadkhah13@gmail.com>
2025-08-17 12:31:58 +0330

    create routes and dashboard component

--- etl_frontend/etl_frontend/src/app/app.routes.ts
+++ etl_frontend/etl_frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+        canMatch: [] //TODO addin auth guard
+    },
+    {
+        path: '**',
+        // redirectTo: //TODO add not found pages 
+    }
+
+];
