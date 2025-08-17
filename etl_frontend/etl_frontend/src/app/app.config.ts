13b6b04245ccb2bd2dc16a389cd5472d2e25682e
amirbarari <amirmohammadbarari415@gmail.com>
2025-08-17 17:31:08 +0330

    cofiguring key-cloak

--- etl_frontend/etl_frontend/src/app/app.config.ts
+++ etl_frontend/etl_frontend/src/app/app.config.ts
@@ -1,18 +1,36 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { includeBearerTokenInterceptor, provideKeycloak, } from 'keycloak-angular';
+
 
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-  providePrimeNG({
-    theme: {
-      preset: Aura,
-    },
-    ripple: true
-  })
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
+    provideKeycloak({
+      config: {
+        clientId: "my_frontend_app",
+        realm: 'codestar',
+        url: "192.168.25.178:8080",
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        checkLoginIframe: true,
+      },
+    }),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+      ripple: true
+    }
+    )
   ]
 };
