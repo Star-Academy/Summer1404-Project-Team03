2c79c07015e3d42cce0ec2e6b661ffe38eda5cd6
amirbarari <amirmohammadbarari415@gmail.com>
2025-08-18 21:43:13 +0330

    creating not-found component and setup wild route

--- etl_frontend/src/app/app.routes.ts
+++ etl_frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -10,16 +11,19 @@ export const routes: Routes = [
     path: 'dashboard',
     loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
     canMatch: [], //TODO check this
-    data: { role: "data_admin"}
+    data: { role: "data_admin" }
   },
   {
     path: 'landing',
     loadComponent: () => import('./features/landing/landing.component').then(m => m.LandingComponent),
     canActivate: [] //TODO adding auth guard
   },
-  // {
-  //     path: '**',
-  //     // redirectTo: //TODO add not found pages
-  // }
-
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  }
 ];
