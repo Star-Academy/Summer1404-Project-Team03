b349faebccb97fbac7aab6a5bf86b73d9e0da674
Mohammad Amin Dadkhah <mohammadamin.dadkhah13@gmail.com>
2025-08-19 17:07:40 +0330

    fix: btn disabled during loading api

--- etl_frontend/src/app/shared/directives/sign-in.directive.ts
+++ etl_frontend/src/app/shared/directives/sign-in.directive.ts
@@ -1,7 +1,7 @@
-import {Directive, HostBinding, HostListener, signal} from '@angular/core';
+import {Directive, effect, Host, HostListener, signal} from '@angular/core';
 import {AuthService} from '../services/auth/auth.service';
 import {MessageService} from 'primeng/api';
-import {Router} from '@angular/router';
+import {Button} from 'primeng/button';
 
 @Directive({
   selector: '[appSignIn]',
@@ -9,29 +9,23 @@ import {Router} from '@angular/router';
 })
 export class SignInDirective {
   constructor(
+    @Host() private myComponent: Button,
     private readonly AuthService: AuthService,
-    private readonly messageService: MessageService,
-    private readonly router: Router) {
+    private readonly messageService: MessageService
+  ) {
+    effect(() => {
+      this.myComponent.loading = this.isLoading();
+    });
   }
 
   public isLoading = signal<boolean>(false);
-
-  @HostBinding('loading')
-  get loading() {
-    return this.isLoading();
-  }
-
-  // ✅ disable button when loading
-  @HostBinding('disabled')
-  get disabled() {
-    return this.isLoading();
-  }
 
   @HostListener('click')
   onSignIn() {
     if (this.isLoading()) return;
 
     this.isLoading.set(true);
+    this.myComponent.loading = true;
     this.AuthService.getSignInUrl().subscribe({
       next: (res: { redirectUrl: string }) => {
         this.isLoading.set(false);
