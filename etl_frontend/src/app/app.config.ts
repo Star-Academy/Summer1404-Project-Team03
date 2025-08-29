6040cf4797f4fd163e2920a7d46586f59d70999f
Mohammad Amin Dadkhah <mohammadamin.dadkhah13@gmail.com>
2025-08-29 16:17:55 +0330

    feat: add interceptor for http request

--- etl_frontend/src/app/app.config.ts
+++ etl_frontend/src/app/app.config.ts
@@ -1,34 +1,40 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store'
+  import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+  import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+  import { providePrimeNG } from 'primeng/config';
+  import { provideRouter } from '@angular/router';
+  import { routes } from './app.routes';
+  import { provideStore } from '@ngrx/store'
 
-import { CustomAura } from './themes/custome-aura';
+  import { CustomAura } from './themes/custome-aura';
 
-import { CoreModule } from './core/core.module';
-import { provideHttpClient } from '@angular/common/http';
+  import { CoreModule } from './core/core.module';
+  import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+  import {CredentialsInterceptor} from './shared/inceptors/crendential.interceptor'
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    importProvidersFrom(CoreModule),
-    provideAnimationsAsync(),
-    provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: CustomAura,
-        options: {
-          prefix: 'p',
-          darkModeSelector: 'dark-mode',
-          cssLayer: false,
+  export const appConfig: ApplicationConfig = {
+    providers: [
+      importProvidersFrom(CoreModule),
+      provideAnimationsAsync(),
+      provideZoneChangeDetection({eventCoalescing: true}),
+      provideRouter(routes),
+      provideAnimationsAsync(),
+      providePrimeNG({
+        theme: {
+          preset: CustomAura,
+          options: {
+            prefix: 'p',
+            darkModeSelector: 'dark-mode',
+            cssLayer: false,
+          },
         },
+        ripple: true,
+      }),
+      provideStore(),
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: CredentialsInterceptor,
+        multi: true,
       },
-      ripple: true,
-    }),
-    provideStore(),
-    provideHttpClient()
-  ],
-};
+      provideHttpClient(withInterceptorsFromDi())
+    ],
+  };
