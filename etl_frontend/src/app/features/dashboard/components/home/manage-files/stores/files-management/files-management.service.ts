3d86711b015b585d21bdc5d495be5babc737790b
amirbarari <amirmohammadbarari415@gmail.com>
2025-09-07 10:00:15 +0330

    create fileSet updater and complete fetching files implementation

--- etl_frontend/src/app/features/dashboard/components/home/manage-files/stores/files-management/files-management.service.ts
+++ etl_frontend/src/app/features/dashboard/components/home/manage-files/stores/files-management/files-management.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
-import { FileManagmentState } from '../../models/file.model';
+import { FileItem, FileManagmentState } from '../../models/file.model';
 import { finalize, switchMap, tap } from 'rxjs';
 import { FilesManagementService } from '../../services/files-management/files-management.service';
 
@@ -23,12 +23,19 @@ export class FilesManagementStore extends ComponentStore<FileManagmentState> {
     isLoadingFiles: value,
   }));
 
-  public readonly getFiles = this.effect<void>(($trigger) =>
-    $trigger.pipe(
+  public readonly setFiles = this.updater((state, files: FileItem[]) => ({
+    ...state,
+    fileItems: files,
+  }));
+
+  public readonly getFiles = this.effect<void>(($trigger) => {
+    return $trigger.pipe(
       tap(() => this.setLoadinFiles(true)),
       switchMap(() => this.fileManagementService.fetchFiles().pipe(
+        tap((files: FileItem[]) => this.setFiles(files)),
         finalize(() => this.setLoadinFiles(false)),
       )),
     )
+  }
   );
 }
