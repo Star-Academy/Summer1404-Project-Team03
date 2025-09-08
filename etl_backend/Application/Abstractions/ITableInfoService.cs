aa8d81be658f6fd5882bf08ab43196dcba848d10
SabaSaadatnia <saba171120@gmail.com>
2025-09-08 22:14:15 +0330

    Dockerfile fixed

--- etl_backend/Application/Abstractions/ITableInfoService.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Abstractions;
-
-// public interface ITableInfoService
-// {
-//     Task<TableDetailsDto> GetDetailsAsync(int schemaId, CancellationToken ct = default);
-//     Task<RowPreviewDto> PreviewRowsAsync(int schemaId, int offset, int limit, string? orderBy, string? direction, CancellationToken ct = default);
-//     Task<RowCountDto> GetRowCountAsync(int schemaId, bool exact, CancellationToken ct = default);
-// }
