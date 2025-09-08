a08961eacfb519530bf236d35c086774a01c0005
amirbarari <amirmohammadbarari415@gmail.com>
2025-09-08 20:03:15 +0330

    revert removed uploader-file component and fix some folders structure

--- etl_frontend/src/app/features/dashboard/components/home/manage-files/components/add-new-file/add-new-file.component.ts
+++ etl_frontend/src/app/features/dashboard/components/home/manage-files/components/add-new-file/add-new-file.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { UploadFileStore } from './stores/upload-file/upload-file-store.service';
 import { RouterOutlet } from '@angular/router';
-import { FileProcessStepperComponent } from '../../components/add-new-file/components/file-process-stepper/file-process-stepper.component'
-
+import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
 @Component({
   selector: 'app-add-new-file',
   templateUrl: './add-new-file.component.html',
-  imports: [FileProcessStepperComponent, RouterOutlet],
+  imports: [FileUploaderComponent, RouterOutlet],
   providers: [UploadFileStore],
   styleUrl: './add-new-file.component.scss',
 })
---
+++ etl_frontend/src/app/features/dashboard/components/home/manage-files/components/add-new-file/components/file-uploader/file-uploader.component.ts
@@ -0,0 +1,110 @@
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Button } from 'primeng/button';
+import { animate, style, transition, trigger } from "@angular/animations"
+import { RouterLink } from '@angular/router';
+import { UploadFileStore } from '../../stores/upload-file/upload-file-store.service';
+import { ConfirmationService } from 'primeng/api';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { UploadedFileItemComponent } from './components/uploaded-file-item/uploaded-file-item.component';
+
+
+@Component({
+  selector: 'app-file-uploader',
+  imports: [Button, NgClass, UploadedFileItemComponent, RouterLink, ProgressBarModule],
+  templateUrl: './file-uploader.component.html',
+  styleUrl: './file-uploader.component.scss',
+  animations: [
+    trigger('scaleAnimation', [
+      transition(':leave', [
+        animate(
+          '150ms ease-in',
+          style({ transform: 'scale(0.8)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
+})
+export class FileUploaderComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  public readonly vm;
+
+  constructor(
+    private readonly uploadFileStore: UploadFileStore,
+    private readonly confirmationService: ConfirmationService) {
+    this.vm = this.uploadFileStore.vm;
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.stopPropagation();
+    event.preventDefault();
+    this.uploadFileStore.setDragging(true);
+  }
+
+  onDragLeave(event: DragEvent): void {
+    event.stopPropagation();
+    event.preventDefault();
+    this.uploadFileStore.setDragging(false);
+  }
+
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.uploadFileStore.setDragging(false);
+
+    const droppedFiles = event.dataTransfer?.files;
+
+    if (droppedFiles) {
+      const files = Array.from(droppedFiles);
+      files.forEach((file: File) => this.handleFile(file));
+    }
+  }
+
+  onFileSelect(event: Event): void {
+    const element = event.target as HTMLInputElement;
+    const selectedFiles = element.files;
+
+    if (selectedFiles) {
+      Array.from(selectedFiles).forEach((file) =>
+        this.handleFile(file)
+      );
+    }
+
+    element.value = '';
+  }
+
+  removeFile(fileName: string): void {
+    this.uploadFileStore.removeFile(fileName);
+  }
+
+  clearFiles(): void {
+    this.uploadFileStore.clearFiles();
+  }
+
+  handleFile(file: File) {
+    const currentFiles = this.uploadFileStore.vm().files;
+    const exists = currentFiles.some(f => f.name === file.name);
+
+    if (exists) {
+      this.showReplacFileConfirmation(file);
+    } else {
+      this.uploadFileStore.addFile(file);
+    }
+  }
+
+  showReplacFileConfirmation(file: File): void {
+    this.confirmationService.confirm({
+      message: `File "${file.name}" already exists. Replace it?`,
+      header: 'Duplicate File',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Replace',
+      rejectLabel: 'Ignore',
+      accept: () => this.uploadFileStore.replaceFile(file),
+      reject: () => { }
+    });
+  }
+
+  onUploadAllFiles(): void {
+    this.uploadFileStore.uploadFiles();
+  }
+}
