a803a650f266730cc13e5e1a41bc7bc618dbb815
amirbarari <amirmohammadbarari415@gmail.com>
2025-09-11 19:23:16 +0330

    fix table detail route

--- etl_frontend/src/app/features/dashboard/components/home/manage-tables/manage-tables.routing.ts
+++ etl_frontend/src/app/features/dashboard/components/home/manage-tables/manage-tables.routing.ts
@@ -5,20 +5,18 @@ import { TableColumnComponent } from "./components/table-column/table-column.com
 
 
 export const manageTablesRoutes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'list'
-    },
-    {
-        path: 'list',
-        component: ManageTablesComponent,
-        children: [
-          {
-            path: ':table-id',
-            loadComponent: () =>
-              import('./components/table-column/table-column.component').then(c => c.TableColumnComponent)
-          }
-        ]
-    },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list'
+  },
+  {
+    path: 'list',
+    component: ManageTablesComponent,
+  },
+  {
+    path: ':table-id',
+    loadComponent: () =>
+      import('./components/table-column/table-column.component').then(c => c.TableColumnComponent)
+  }
 ]
