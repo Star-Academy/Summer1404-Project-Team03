e736d8b3d75ffafeb2a22db89fe443ea9f898caf
amirbarari <amirmohammadbarari415@gmail.com>
2025-09-14 12:32:18 +0330

    fix default sign-in directive test file

--- etl_frontend/src/app/shared/directives/sign-in/sign-in.directive.spec.ts
+++ etl_frontend/src/app/shared/directives/sign-in/sign-in.directive.spec.ts
@@ -1,8 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignInDirective } from './sign-in.directive';
+import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+
+@Component({
+  template: `<button pButton appSignIn></button>`
+})
+class TestHostComponent { }
 
 describe('SignInDirective', () => {
+  let fixture: ComponentFixture<TestHostComponent>;
+  let directiveEl: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestHostComponent, SignInDirective],
+      imports: [Button]
+    })
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    fixture.detectChanges();
+    directiveEl = fixture.nativeElement.querySlector('button');
+  })
+
   it('should create an instance', () => {
-    const directive = new SignInDirective();
-    expect(directive).toBeTruthy();
+    expect(directiveEl).toBeTruthy();
   });
 });
