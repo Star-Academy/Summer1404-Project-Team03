cf0315b722edea5ff888d41d8ef155eec2a8bda6
Mohammad Amin Dadkhah <mohammadamin.dadkhah13@gmail.com>
2025-09-14 13:20:47 +0330

    feat: add karma for testing

--- etl_frontend/src/app/shared/directives/change-password/change-password.directive.spec.ts
+++ etl_frontend/src/app/shared/directives/change-password/change-password.directive.spec.ts
@@ -1,8 +1,23 @@
+import {ChangeDetectorRef, Renderer2 } from '@angular/core';
 import { ChangePasswordDirective } from './change-password.directive';
+import { Button } from 'primeng/button';
+import { UsersService } from '../../services/user/users.service';
+import { MessageService } from 'primeng/api';
 
 describe('ChangePasswordDirective', () => {
   it('should create an instance', () => {
-    const directive = new ChangePasswordDirective();
+    const mockButton = {} as Button;
+    const mockUsersService = {} as UsersService;
+    const mockConfig = {} as MessageService;
+    const mockRenderer = {} as ChangeDetectorRef;
+
+    const directive = new ChangePasswordDirective(
+      mockButton,
+      mockUsersService,
+      mockConfig,
+      mockRenderer
+    );
+
     expect(directive).toBeTruthy();
   });
 });
--- etl_frontend/src/app/shared/directives/sign-out/sign-out.directive.spec.ts
+++ etl_frontend/src/app/shared/directives/sign-out/sign-out.directive.spec.ts
@@ -1,8 +1,23 @@
+import { Button } from 'primeng/button';
+import { MessageService } from 'primeng/api';
 import { SignOutDirective } from './sign-out.directive';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 describe('SignOutDirective', () => {
   it('should create an instance', () => {
-    const directive = new SignOutDirective();
+    const mockButton = {} as Button;
+    const mockAuthService = {} as AuthService;
+    const mockMessage = {} as MessageService;
+    const mockRouter = {} as Router;
+
+    const directive = new SignOutDirective(
+      mockButton,
+      mockAuthService,
+      mockMessage,
+      mockRouter
+    );
+
     expect(directive).toBeTruthy();
   });
 });
