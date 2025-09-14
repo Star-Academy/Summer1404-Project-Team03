95643b75e6d5f6a2e64c2b5fff512046a8c40a85
amirbarari <amirmohammadbarari415@gmail.com>
2025-09-14 14:19:23 +0330

    reduce dupplication in scheck-route pipe test

--- etl_frontend/src/app/shared/pipe/check-route.pipe.spec.ts
+++ etl_frontend/src/app/shared/pipe/check-route.pipe.spec.ts
@@ -1,24 +1,29 @@
 import { CheckRoutePipe } from './check-route.pipe';
 import { Router } from '@angular/router';
 
-describe('CheckRoutePipe', () => {
-  it('create an instance', () => {
-    const mockRouter = { url: '/home' } as Router;
-    const pipe = new CheckRoutePipe(mockRouter);
+fdescribe('CheckRoutePipe', () => {
+  let currentUrl: string;
+  let mockRouter: Partial<Router>;
+  let pipe: CheckRoutePipe;
+
+  beforeEach(() => {
+    currentUrl = '/home';
+    mockRouter = {
+      get url() { return currentUrl; }
+    };
+    pipe = new CheckRoutePipe(mockRouter as Router);
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
   it('should return true if route matches', () => {
-    const mockRouter = { url: '/home' } as Router;
-    const pipe = new CheckRoutePipe(mockRouter);
-
     expect(pipe.transform('/home')).toBeTrue();
   });
 
   it('should return false if route does not match', () => {
-    const mockRouter = { url: '/about' } as Router;
-    const pipe = new CheckRoutePipe(mockRouter);
-
+    currentUrl = '/about';
     expect(pipe.transform('/home')).toBeFalse();
   });
 });
