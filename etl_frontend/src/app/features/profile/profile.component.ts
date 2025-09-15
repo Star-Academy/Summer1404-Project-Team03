c8666ff72327200e2ff079c6d0ac7009bea31587
amirbarari <amirmohammadbarari415@gmail.com>
2025-09-15 12:54:16 +0330

    update profile component to use hasRole directive

--- etl_frontend/src/app/features/profile/profile.component.ts
+++ etl_frontend/src/app/features/profile/profile.component.ts
@@ -9,9 +9,6 @@ import {UserStoreService} from '../../shared/stores/user-store.service';
 })
 export class ProfileComponent {
   public readonly user = computed(() => this.userStore.vm().user);
-  public readonly isSysAdmin = computed(() =>
-    this.userStore.vm().user.roles.some(role => role.name === 'sys_admin')
-  );
 
   constructor(private readonly userStore: UserStoreService) {
     if (this.user().firstName === '') this.userStore.loadUser();
